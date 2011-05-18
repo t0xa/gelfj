6e3cf420b0dcadf344670de080bb0d2b33018d9b
Jochen Schalanda <jochen@schalanda.name>
2011-05-18 21:47:48 +0200

    Added default value for property originHost: the local host's hostname

--- src/main/java/org/graylog2/log/GelfAppender.java
+++ src/main/java/org/graylog2/log/GelfAppender.java
@@ -1,7 +1,6 @@
 package org.graylog2.log;
 
 import org.apache.log4j.AppenderSkeleton;
-import org.apache.log4j.Layout;
 import org.apache.log4j.Level;
 import org.apache.log4j.spi.ErrorCode;
 import org.apache.log4j.spi.LocationInfo;
@@ -10,15 +9,12 @@ import org.apache.log4j.spi.ThrowableInformation;
 import org.graylog2.GelfMessage;
 import org.graylog2.GelfSender;
 import org.json.simple.JSONValue;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.lang.reflect.InvocationTargetException;
+import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -34,10 +30,30 @@ public class GelfAppender extends AppenderSkeleton {
     private Map<String, String> fields;
 
     private static final int MAX_SHORT_MESSAGE_LENGTH = 250;
-    private final String ORIGIN_HOST_KEY = "originHost";
+    private static final String ORIGIN_HOST_KEY = "originHost";
+
+    public GelfAppender() {
+
+        super();
+
+        this.originHost = getLocalHostName();
+    }
+
+    private String getLocalHostName() {
+
+        String hostName = null;
+
+        try {
+            hostName = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            errorHandler.error("Unknown local hostname", e, ErrorCode.GENERIC_FAILURE);
+        }
+
+        return hostName;
+    }
 
     public void setAdditionalFields(String additionalFields) {
-         fields = (HashMap) JSONValue.parse(additionalFields.replaceAll("'", "\""));
+         fields = (Map<String, String>) JSONValue.parse(additionalFields.replaceAll("'", "\""));
     }
 
     public int getGraylogPort() {
