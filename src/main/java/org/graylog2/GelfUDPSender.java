1f63fa83ba5dd6157f778f893477d9cd8b45294f
Hans-Joachim Kliemeck <klk@mmw.ag>
2012-11-09 11:30:37 +0100

    separate between udp and tcp sender

---
+++ src/main/java/org/graylog2/GelfUDPSender.java
@@ -0,0 +1,57 @@
+package org.graylog2;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+public class GelfUDPSender implements GelfSender {
+
+	public static final int DEFAULT_PORT = 12201;
+	private InetAddress host;
+	private int port;
+	private DatagramChannel channel;
+
+	public GelfUDPSender(String host) throws IOException {
+		this(host, DEFAULT_PORT);
+	}
+
+	public GelfUDPSender(String host, int port) throws IOException {
+		this.host = InetAddress.getByName(host);
+		this.port = port;
+		this.channel = initiateChannel();
+	}
+
+	private DatagramChannel initiateChannel() throws IOException {
+		DatagramChannel resultingChannel = DatagramChannel.open();
+		resultingChannel.socket().bind(new InetSocketAddress(0));
+		resultingChannel.connect(new InetSocketAddress(this.host, this.port));
+		resultingChannel.configureBlocking(false);
+
+		return resultingChannel;
+	}
+
+	public boolean sendMessage(GelfMessage message) {
+		return message.isValid() && sendDatagrams(message.toBuffers());
+	}
+
+	private boolean sendDatagrams(ByteBuffer[] bytesList) {
+		try {
+			for (ByteBuffer buffer : bytesList) {
+				channel.write(buffer);
+			}
+		} catch (IOException e) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public void close() {
+		try {
+			channel.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
