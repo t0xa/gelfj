3fd71a97c5a07196821be2b37bbd5c61f3154ce6
Anton Yakimov <anton.jakimov@gmail.com>
2013-08-30 13:55:09 +0300

    Merge branch 'amqp' of https://github.com/h0nIg/gelfj into h0nIg-amqp
    Conflicts:
    	src/main/java/org/graylog2/logging/GelfHandler.java

--- src/test/java/org/graylog2/logging/LoggingTresholdTest.java
+++ src/test/java/org/graylog2/logging/LoggingTresholdTest.java
@@ -1,4 +1,17 @@
 package org.graylog2.logging;
 
+import org.junit.Test;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class LoggingTresholdTest {
+
+    @Test
+    public void testLogFormattingWithParameter() {
+        Logger myLogger = Logger.getLogger("org.graylog2.TestLogger");
+        myLogger.log(Level.FINE, "Testing FINE message");
+        myLogger.log(Level.INFO, "Testing INFO message");
+    }
+
 }
