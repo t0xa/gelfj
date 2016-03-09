8c3d3843f84da013110ab8945abef2bd24495701
James Burton <the.james.burton@gmail.com>
2016-03-09 14:33:33 +0000

    added GelfJsonAppender and docs for it

---
+++ src/main/java/org/graylog2/log/GelfJsonAppender.java
@@ -0,0 +1,45 @@
+package org.graylog2.log;
+
+import java.util.Map;
+
+import org.apache.log4j.spi.ErrorCode;
+import org.apache.log4j.spi.LoggingEvent;
+import org.graylog2.GelfMessage;
+import org.graylog2.GelfMessageFactory;
+import org.graylog2.GelfSenderResult;
+import org.json.simple.JSONValue;
+
+/**
+ * A GelfAppender which will parse the given JSON message into additional fields in GELF
+ *
+ * @author Anton Yakimov
+ * @author Jochen Schalanda
+ * @author the-james-burton
+ */
+public class GelfJsonAppender extends GelfAppender {
+
+  @Override
+  protected void append(final LoggingEvent event) {
+    GelfMessage gelfMessage = GelfMessageFactory.makeMessage(layout, event, this);
+
+    @SuppressWarnings("unchecked")
+    Map<String, String> fields = (Map<String, String>) JSONValue.parse(event.getMessage().toString());
+
+    if (fields != null) {
+      for (String key : fields.keySet()) {
+        gelfMessage.addField(key, fields.get(key));
+      }
+    }
+
+    if (getGelfSender() == null) {
+      errorHandler.error("Could not send GELF message. Gelf Sender is not initialised and equals null");
+    } else {
+      GelfSenderResult gelfSenderResult = getGelfSender().sendMessage(gelfMessage);
+      if (!GelfSenderResult.OK.equals(gelfSenderResult)) {
+        errorHandler.error("Error during sending GELF message. Error code: " + gelfSenderResult.getCode() + ".",
+            gelfSenderResult.getException(), ErrorCode.WRITE_FAILURE);
+      }
+    }
+  }
+
+}
