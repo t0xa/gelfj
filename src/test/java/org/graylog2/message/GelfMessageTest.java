534d0eb765f5f2fbef1fe7a7416206b971a86ee6
Sami Korhonen <skorhone@gmail.com>
2016-09-06 09:55:18 +0300

    Clean up: Combined two unit test classes that tested same class

--- src/test/java/org/graylog2/message/GelfMessageTest.java
+++ src/test/java/org/graylog2/message/GelfMessageTest.java
@@ -5,12 +5,16 @@ import static junit.framework.Assert.assertNull;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
 
 public class GelfMessageTest {
     @Test
@@ -77,5 +81,49 @@ public class GelfMessageTest {
         JSONObject object = (JSONObject) JSONValue.parse(message.toJson());
 
         assertThat("Message with invalid level defaults to info", (Long) object.get("level"), is(6L));
+    }
+    
+    @Test
+    public void testMutability() {
+        GelfMessage message = new GelfMessage("Short", "Long", new Date().getTime(), "1");
+
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("id", "id");
+        map.put("x", "y");
+
+        message.setAdditonalFields(map);
+
+        message.addField("idz", "LOLCAT").addField("_id", "typos in my closet");
+
+        assertThat("Modifying original map doesn't modify message map", map.size(), is(2));
+        assertThat("Modifying original map ", message.getAdditonalFields().size(), is(4));
+    }
+
+    @Test
+    public void testJavaImmutableMap() {
+        GelfMessage message = new GelfMessage("Short", "Long", new Date().getTime(), "1");
+
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("id", "id");
+        map.put("x", "y");
+        Map<String, Object> additonalFields = Collections.unmodifiableMap(map);
+
+        message.setAdditonalFields(additonalFields);
+
+        message.addField("idz", "LOLCAT").addField("_id", "typos in my closet");
+
+        assertThat("If created from Immutable map, it should be possible to add fields", message.getAdditonalFields().size(), is(4));
+    }
+
+    @Test
+    public void testGuavaImmutableMaps() {
+        GelfMessage message = new GelfMessage("Short", "Long", new Date().getTime(), "1");
+
+        ImmutableMap<String, Object> build = ImmutableMap.<String, Object>builder().put("id", "id").put("x", "y").build();
+        message.setAdditonalFields(build);
+
+        message.addField("idz", "LOLCAT").addField("_id", "typos in my closet");
+
+        assertThat("Modifying original map doesn't modify message map", message.getAdditonalFields().size(), is(4));
     }
 }
